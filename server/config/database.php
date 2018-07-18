6c38f2a700ce06c999bf4eab311bdbd24435e8fc
Clelo4 <chengjunjie.jack@gmail.com>
2018-07-18 19:48:15 +0800

    Update database.php
    删除重要密码

--- server/config/database.php
+++ server/config/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '123.207.35.163',
+    'hostname'        => ''
     // 数据库名
-    'database'        => 'actsboard',
+    'database'        => '',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => 'ehDj7997Dharma79.p69',
+    'password'        => '',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
